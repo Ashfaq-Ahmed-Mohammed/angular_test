e3035096f1504216d05364c9e5fff88cd7e14141
Ashfaaq ahmed <ashfaaqahmed09@gmail.com>
2025-01-20 02:34:51 -0500

    Angular practice

--- angular_project/src/app/app.component.ts
+++ angular_project/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalculatorComponent } from "./calculator/calculator.component";
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CalculatorComponent, MatSlideToggleModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
