4f888465af79eceb87686b972649bfc8f533b16e
Ashfaq-Ahmed-Mohammed <ashfaaqahmed09@icloud.com>
2025-01-21 23:41:52 -0500

    Added calcualtor layout

--- angular_project/src/app/calculator/calculator.component.ts
+++ angular_project/src/app/calculator/calculator.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { FlexLayoutModule } from '@ngbracket/ngx-layout';
+
+
 
 @Component({
   selector: 'app-calculator',
-  imports: [],
+  imports: [MatButtonModule, FlexLayoutModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.scss'
 })
 export class CalculatorComponent {
+  displayValue = '';
 }
